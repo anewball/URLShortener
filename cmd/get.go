a5540314bace2eab1b733ea84c978f3e7d35a122
Andy Newball <andynewball@gmail.com>
2025-08-25 13:49:13 -0400

    creating function getAction, variable getActionFunc

--- cmd/get.go
+++ cmd/get.go
@@ -3,31 +3,43 @@ package cmd
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
+	"io"
 	"time"
 
 	"github.com/spf13/cobra"
 )
 
-func NewGet(a *App) *cobra.Command {
-	c := &cobra.Command{
+var getActionFunc = getAction
+
+func NewGet(app *App) *cobra.Command {
+	return &cobra.Command{
 		Use:   "get <code>",
 		Short: "Retrieve a URL from the shortener service",
 		Args:  cobra.MinimumNArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			ctx, cancel := context.WithTimeout(cmd.Context(), 5*time.Second)
-			defer cancel()
-
-			url, err := a.S.Get(ctx, args[0])
-			if err != nil {
-				return fmt.Errorf("failed to retrieve original URL: %w", err)
-			}
-
-			result := Result{Code: args[0], Url: url}
-			encoder := json.NewEncoder(cmd.OutOrStdout())
-
-			return encoder.Encode(result)
+			return getActionFunc(cmd.Context(), cmd.OutOrStdout(), app, args)
 		},
 	}
-	return c
+}
+
+func getAction(ctx context.Context, out io.Writer, app *App, args []string) error {
+	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+
+	if len(args) == 0 {
+		return errors.New("requires at least 1 arg(s), only received 0")
+	}
+
+	arg := args[0]
+	url, err := app.S.Get(ctx, arg)
+	if err != nil {
+		return fmt.Errorf("failed to retrieve original URL: %w", err)
+	}
+
+	result := Result{Code: arg, Url: url}
+	encoder := json.NewEncoder(out)
+
+	return encoder.Encode(result)
 }
