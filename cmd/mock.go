a5febda5cc952c8a1885388fd6582bdc16e1fb45
Andy Newball <andynewball@gmail.com>
2025-08-26 21:25:28 -0400

    Creating new mock Factory

--- cmd/mock.go
+++ cmd/mock.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/anewball/urlshortener/internal/shortener"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 type mockedShortener struct {
@@ -27,4 +28,17 @@ func (m *mockedShortener) List(ctx context.Context, limit, offset int) ([]shorte
 
 func (m *mockedShortener) Delete(ctx context.Context, code string) (bool, error) {
 	return m.deleteFunc(ctx, code)
+}
+
+type MockFactory struct {
+	ParseConfigFunc   func(string) (*pgxpool.Config, error)
+	NewWithConfigFunc func(context.Context, *pgxpool.Config) (*pgxpool.Pool, error)
+}
+
+func (m *MockFactory) ParseConfig(dns string) (*pgxpool.Config, error) {
+	return m.ParseConfigFunc(dns)
+}
+
+func (m *MockFactory) NewWithConfig(ctx context.Context, cfg *pgxpool.Config) (*pgxpool.Pool, error) {
+	return m.NewWithConfigFunc(ctx, cfg)
 }
