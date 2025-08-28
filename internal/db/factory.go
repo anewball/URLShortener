2cfd7ae4892816e8b01b9f39fe6f0d6ea14a160a
Andy Newball <andynewball@gmail.com>
2025-08-28 17:02:36 -0400

    Creating a factory file that create a new pool

---
+++ internal/db/factory.go
@@ -0,0 +1,15 @@
+package shortener
+
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type Config struct {
+	URL string
+}
+
+func NewPool(ctx context.Context, cfg Config) (*pgxpool.Pool, error) {
+	return pgxpool.New(ctx, cfg.URL)
+}
