2a272b0358494652a3efff6f7a3c4ed56bda97a4
Andy Newball <andynewball@gmail.com>
2025-08-29 14:47:39 -0400

    Adding method Close to interface DatabaseConn

--- internal/shortener/shortener.go
+++ internal/shortener/shortener.go
@@ -17,6 +17,7 @@ type DatabaseConn interface {
 	QueryRow(ctx context.Context, sql string, args ...any) pgx.Row
 	Exec(ctx context.Context, sql string, arguments ...any) (pgconn.CommandTag, error)
 	Query(ctx context.Context, sql string, args ...any) (pgx.Rows, error)
+	Close()
 }
 
 type Shortener interface {
