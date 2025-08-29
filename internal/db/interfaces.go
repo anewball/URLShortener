973df8ee6d68a495bcdf42cf584306d5a18003fc
Andy Newball <andynewball@gmail.com>
2025-08-29 14:56:09 -0400

    Creating a interface that satisfy pgxpool.Pool

---
+++ internal/db/interfaces.go
@@ -0,0 +1,18 @@
+package db
+
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type Conn interface {
+	QueryRow(ctx context.Context, sql string, args ...any) pgx.Row
+	Exec(ctx context.Context, sql string, arguments ...any) (pgconn.CommandTag, error)
+	Query(ctx context.Context, sql string, args ...any) (pgx.Rows, error)
+	Close()
+}
+
+var _ Conn = (*pgxpool.Pool)(nil)
