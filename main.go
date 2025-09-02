64a1767659afd1d8149a136590b08f24409f4451
Andy Newball <andynewball@gmail.com>
2025-09-02 12:29:32 -0400

    Removing yam config

--- main.go
+++ main.go
@@ -2,20 +2,16 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 
 	"github.com/anewball/urlshortener/cmd"
+	"github.com/joho/godotenv"
 	"github.com/spf13/viper"
 )
 
 func main() {
-	viper.SetConfigName("config")
-	viper.SetConfigType("yaml")
-	viper.AddConfigPath(".")
-	if err := viper.ReadInConfig(); err != nil {
-		log.Printf("No config file found, using env only")
-	}
+	godotenv.Load()
+	viper.AutomaticEnv()
 
 	if err := cmd.Run(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
