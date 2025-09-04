f3b6c82a3718a3f52ac33ad5e5ce340f4c1dc9b0
Andy Newball <andynewball@gmail.com>
2025-09-04 12:53:44 -0400

    Returning an error

--- internal/app/app.go
+++ internal/app/app.go
@@ -10,15 +10,15 @@ type App struct {
 	Shortener shortener.Service
 }
 
-func New(conn db.Conn) *App {
+func New(conn db.Conn) (*App, error) {
 	gen := shortener.NewNanoID(shortener.Alphabet)
 
 	short, err := shortener.New(conn, gen)
 	if err != nil {
-		return nil
+		return nil, err
 	}
 
-	return &App{Conn: conn, Shortener: short}
+	return &App{Conn: conn, Shortener: short}, nil
 }
 
 func (app *App) Close() {
