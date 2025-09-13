9a12feb500c72fa6198b52eee067e143f11e692a
Andy Newball <andynewball@gmail.com>
2025-09-13 18:18:58 -0400

    Cleaning up mock

--- cmd/mock.go
+++ cmd/mock.go
@@ -4,10 +4,7 @@ import (
 	"context"
 	"io"
 
-	"github.com/anewball/urlshortener/internal/db"
 	"github.com/anewball/urlshortener/internal/shortener"
-	"github.com/jackc/pgx/v5"
-	"github.com/jackc/pgx/v5/pgconn"
 )
 
 var _ shortener.URLShortener = (*mockedShortener)(nil)
@@ -33,42 +30,6 @@ func (m *mockedShortener) List(ctx context.Context, limit, offset int) ([]shorte
 
 func (m *mockedShortener) Delete(ctx context.Context, code string) (bool, error) {
 	return m.deleteFunc(ctx, code)
-}
-
-var _ db.Conn = (*mockPool)(nil)
-
-type mockPool struct {
-	queryRowFunc func(ctx context.Context, sql string, args ...any) pgx.Row
-	execFunc     func(ctx context.Context, sql string, args ...any) (pgconn.CommandTag, error)
-	queryFunc    func(ctx context.Context, sql string, args ...any) (pgx.Rows, error)
-	closeFunc    func()
-}
-
-func (m *mockPool) QueryRow(ctx context.Context, sql string, args ...any) pgx.Row {
-	if m.queryRowFunc != nil {
-		return m.queryRowFunc(ctx, sql, args...)
-	}
-	return nil
-}
-
-func (m *mockPool) Exec(ctx context.Context, sql string, args ...any) (pgconn.CommandTag, error) {
-	if m.execFunc != nil {
-		return m.execFunc(ctx, sql, args...)
-	}
-	return pgconn.CommandTag{}, nil
-}
-
-func (m *mockPool) Query(ctx context.Context, sql string, args ...any) (pgx.Rows, error) {
-	if m.queryFunc != nil {
-		return m.queryFunc(ctx, sql, args...)
-	}
-	return nil, nil
-}
-
-func (m *mockPool) Close() {
-	if m.closeFunc != nil {
-		m.closeFunc()
-	}
 }
 
 var _ Actions = (*mockedActions)(nil)
