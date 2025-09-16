741b46ddcf8715957191270874bd8dd910cda9a2
Andy Newball <andynewball@gmail.com>
2025-09-16 18:52:58 -0400

    Removing args validation

--- cmd/delete.go
+++ cmd/delete.go
@@ -9,7 +9,6 @@ func NewDelete(acts Actions, svc shortener.URLShortener) *cobra.Command {
 	return &cobra.Command{
 		Use:   "delete <code>",
 		Short: "Delete a URL from the shortener service by short code",
-		Args:  cobra.MinimumNArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			return acts.DeleteAction(cmd.Context(), cmd.OutOrStdout(), svc, args)
 		},
