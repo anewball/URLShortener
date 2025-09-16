e98a6d173f55f57f217376cb0cb5d68a5188b2d8
Andy Newball <andynewball@gmail.com>
2025-09-16 19:33:57 -0400

    Adding Read JSON

--- internal/jsonutil/jsonutil.go
+++ internal/jsonutil/jsonutil.go
@@ -10,3 +10,8 @@ func WriteJSON(w io.Writer, v any) error {
 	enc.SetEscapeHTML(false)
 	return enc.Encode(v)
 }
+
+func ReadJSON(r io.Reader, v any) error {
+	dec := json.NewDecoder(r)
+	return dec.Decode(v)
+}
