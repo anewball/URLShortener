69bfcf5aa492550f53cbca935bd51b4ff26c2dca
Andy Newball <andynewball@gmail.com>
2025-09-16 18:42:49 -0400

    Removing args validation from add command

--- cmd/add.go
+++ cmd/add.go
@@ -9,7 +9,6 @@ func NewAdd(acts Actions, svc shortener.URLShortener) *cobra.Command {
 	return &cobra.Command{
 		Use:   "add <url>",
 		Short: "Save a URL to the shortener service",
-		Args:  cobra.MinimumNArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			return acts.AddAction(cmd.Context(), cmd.OutOrStdout(), svc, args)
 		},
